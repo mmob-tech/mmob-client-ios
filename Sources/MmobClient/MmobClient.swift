0fc793bf3f7fcae21441ae6ad6cd2e852cbebd5a
Blai <blai@mmob.com>
2022-05-09 09:25:59 +0100

    Update naming

--- Sources/MmobClient/MmobClient.swift
+++ Sources/MmobClient/MmobClient.swift
@@ -32,26 +32,26 @@ public struct MmobCustomerInfo {
 }
 
 public struct MmobCompanyConfiguration {
-    public init(cp: String, integration: String, environment: String = "production") {
-        self.cp = cp
-        self.integration = integration
+    public init(cp_id: String, integration_id: String, environment: String = "production") {
+        self.cp_id = cp_id
+        self.integration_id = integration_id
         self.production = environment == "production"
     }
     
-    var cp: String
-    var integration: String
+    var cp_id: String
+    var integration_id: String
     var production: Bool
 }
 
 public struct MmobConfiguration {
-    public init(company: MmobCompanyConfiguration, customer_info: MmobCustomerInfo) {
-        self.company = company
-        self.customer_info = customer_info
+    public init(configuration: MmobCompanyConfiguration, customer: MmobCustomerInfo) {
+        self.configuration = configuration
+        self.customer = customer
     }
 
     
-    var company: MmobCompanyConfiguration
-    var customer_info: MmobCustomerInfo
+    var configuration: MmobCompanyConfiguration
+    var customer: MmobCustomerInfo
 }
 
 public struct MmobClient {
@@ -62,8 +62,8 @@ public struct MmobClient {
         let view =  WKWebView()
         
 
-        let customer_info = mmobConfiguration.customer_info
-        let company = mmobConfiguration.company
+        let customer_info = mmobConfiguration.customer
+        let company = mmobConfiguration.configuration
         
 
         // Create parameters object
@@ -76,8 +76,8 @@ public struct MmobClient {
             "address_1": customer_info.address_1,
             "town_city": customer_info.town_city,
             "postcode": customer_info.postcode,
-            "cp_id": company.cp,
-            "cp_deployment_id": company.integration
+            "cp_id": company.cp_id,
+            "cp_deployment_id": company.integration_id
         ]
         
         
